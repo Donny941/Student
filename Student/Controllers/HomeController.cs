651d449c63a47b9d11aa3b204c407e03bc9c14a3
Alan Donati <donniesplat@gmail.com>
2025-12-10 15:58:28 +0100

    day 2

--- Student/Controllers/HomeController.cs
+++ Student/Controllers/HomeController.cs
@@ -15,32 +15,61 @@ namespace StudentDashboard.Controllers
         {
             _logger = logger;
             _studentService = studentService;
-
         }
 
         public IActionResult Index()
         {
-
-
             return View();
         }
 
-        public IActionResult Create()
-        {
-            return View("_Create");
-        }
-        public async Task<IActionResult> AddingStudent(StudentDashboard.Models.Entities.Student student)
+        [HttpGet]
+        public async Task<JsonResult> GetStudents()
         {
             try
             {
-                await _studentService.AddStudent(student);
+                var students = await _studentService.GetStudentAsync();
+                return new JsonResult(new { data = students });
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                _logger.LogError(ex, "Errore durante il recupero degli studenti");
+                return new JsonResult(new { data = new List<StudentDashboard.Models.Entities.Student>() });
             }
-            return View("index");
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AddingStudent([FromBody] StudentDashboard.Models.Entities.Student student)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return Json(new { success = false, message = "Dati non validi" });
+                }
+
+                var result = await _studentService.AddStudent(student);
+
+                if (result)
+                {
+                    return Json(new
+                    {
+                        success = true,
+                        message = "Studente aggiunto con successo!",
+                        studentId = student.Id
+                    });
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Errore durante il salvataggio" });
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Errore durante l'aggiunta dello studente");
+                return Json(new { success = false, message = "Si è verificato un errore: " + ex.Message });
+            }
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
